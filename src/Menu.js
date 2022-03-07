49abd3ce9c32a322aa31b9a926af10c59d8105b3
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2022-03-08 01:12:59 +0900

    Menu Filters and add details button

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({items}) => {
+  return ( 
+    <div className='section-center'> 
+      {items.map((menuItem)=>{
+        const {id, title, category, price, img, desc} = menuItem;
+        return (
+          <article key={id} className='menu-item'>
+            <img src={img} alt={title} className='photo' />
+            <div className='item-info'>
+              <header>
+                <h4>{title}</h4>
+                <h4 className='price'>{price}</h4>
+              </header>
+              <p className='item-text'>{desc.substring(0,150)}</p>
+              <button className='filter-btn details'>Details</button>
+            </div>
+          </article>
+          )
+      })}    
+    </div>
+  )
 };
 
 export default Menu;
